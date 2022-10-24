3edb0178cdaabbcfa73c6cf79298108459e774ad
jamesvinnicombe <80378047+jvinn@users.noreply.github.com>
2022-10-24 13:21:11 +1100

    Basic project structure.

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import '../styles.css';
+
+function Header() {
+	return(
+		<div>
+			<div className="header">
+				<text>header</text>
+			</div>
+			<div className="body">
+				<text>body</text>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
