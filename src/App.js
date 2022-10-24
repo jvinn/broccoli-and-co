44aa1eb28bc2fafc8b092e86b047a157b4edb080
jamesvinnicombe <80378047+jvinn@users.noreply.github.com>
2022-10-24 14:50:20 +1100

    Completed home page body and footer styling.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './components/Header';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 		  <div className="body">
 			  <text></text>
 		  </div>
+		  <Footer />
 	  </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import '../styles.css';
+
+function Footer() {
+	return(
+		<div className="footer">
+			<text>Created by James Vinnicombe</text>
+		</div>
+	);
+}
+
+export default Footer;
